753f1acaed781b68189db53dc88d949d639fad7d
Eric <ericwangqing@gmail.com>
2010-12-03 16:44:05 +0800

    before refactoring

---
+++ src/junit/framework/Assert.java
@@ -0,0 +1,10 @@
+package junit.framework;
+
+public class Assert {
+
+	public static String format(String message, String expected, String actual) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
